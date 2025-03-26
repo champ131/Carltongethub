d2c739f1200eebf4c1efbb8f9b42ed0a98d19d57
Powell <carlton.powell@accenture.com>
2025-03-26 15:47:27 -0400

    put my first  pyhton comitt

---
+++ hello.py
@@ -0,0 +1,39 @@
+# %%
+name = input("what's your name?")
+age = input("How old are you?")
+
+print("Hello, " + name + "!")
+print()
+print("You are " + age +" years old.")
+
+
+name = input("what's your name?")
+age = input("How old are you?")
+
+print("Hello, " + name + "!")
+print()
+print("You are " + age +" years old.")
+
+
+name = input("what's your name?")
+age = input("How old are you?")
+
+print("Hello, " + name + "!")
+print()
+print("You are " + age +" years old.")
+
+
+name = input("what's your name?")
+age = input("How old are you?")
+
+print("Hello, " + name + "!")
+print()
+print("You are " + age +" years old.")
+      
+
+####### 340.2.1 operators 
+
+
+a = 32    # Initialize the value of a  
+b = 6      # Initialize the value of b  
+print('Addition of two numbers:',a+b)  
